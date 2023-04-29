f2f2509d7ecf80f7aa6458a9d1fa1b38926a6c32
Kire1919 <131976754+Kire1919@users.noreply.github.com>
2023-04-30 01:04:32 +0200

    Add files via upload

---
+++ arboles.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# In[15]:
+
+
+import tkinter as tk
+
+# Función que genera el nombre de árbol a partir del nombre ingresado
+def generar_nombre_arbol():
+    arboles = ["abedul", "abeto", "acacia", "acebo", "achicoria", "álamo", "árbol del pan", "arce", "avellano", "azufaifo", "baobab", "baño de oro", "bérbero", "brezo", "cactus", "caja", "calabaza", "camomila", "cedro", "cerezo", "cidro", "ciruela", "clavo", "coco", "corteza de roble", "corteza de sauce", "cresta de gallo", "crotón", "curuba", "dátil", "eucalipto", "falso pimentero", "fresno", "frutilla", "ginkgo", "girasol", "granado", "grosella espinosa", "guayaba", "henna", "hibisco", "higuera", "hinojo", "hisopo", "holly", "jacaranda", "jazmín", "jengibre", "jícara", "junco", "laurel", "lavanda", "lima", "lúpulo", "magnolia", "mandarina", "manzano", "maracuyá", "margarita", "menta", "mezquite", "mimosa", "mora", "moringa", "muérdago", "naranjo", "nogal", "olivo", "palma", "paraíso", "papaya", "peral", "perejil", "pino", "plátano", "plumeria", "pomelo", "poncirus", "porotillo", "quenepa", "quillay", "rábano", "rosa", "rosal silvestre", "sándalo", "sauce", "siempreviva", "spathodea", "tamarindo", "tangelo", "tejo", "temu", "tomate", "toronja", "uva", "vainilla", "vid", "yaca", "ylang ylang", "yuca"]
+    nombre = entrada_nombre.get()
+    suma_ascii = sum(ord(c) for c in nombre)
+    indice_arbol = suma_ascii % len(arboles)
+    resultado_nombre_arbol.configure(text="Tu nombre de árbol es: " + arboles[indice_arbol])
+
+# Crear la ventana principal y los widgets
+ventana = tk.Tk()
+ventana.title("Generador de nombres de árboles")
+etiqueta_nombre = tk.Label(ventana, text="Ingresa tu nombre:")
+entrada_nombre = tk.Entry(ventana)
+boton_generar = tk.Button(ventana, text="Generar nombre de árbol", command=generar_nombre_arbol)
+resultado_nombre_arbol = tk.Label(ventana, text="")
+
+# Ubicar los widgets en la ventana
+etiqueta_nombre.pack()
+entrada_nombre.pack()
+boton_generar.pack()
+resultado_nombre_arbol.pack()
+
+# Iniciar el bucle principal de la ventana
+ventana.mainloop()
+
+
+# In[ ]:
+
+
+
+
