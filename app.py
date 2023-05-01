9f4ff164d55e369cfbfe08316be4a6d800f9ed17
Kire1919 <131976754+Kire1919@users.noreply.github.com>
2023-05-01 19:37:19 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/arboles')
+def arboles():
+    nombre = request.args.get('nombre', '')
+    try:
+        resultado = subprocess.check_output(['python', 'arboles.py', nombre])
+        return jsonify({'arbol': resultado.decode().strip()})
+    except subprocess.CalledProcessError:
+        return jsonify({'error': 'No se ha podido generar el árbol'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
