44bd20e22dc2e5d7ea00ca9cc4df12553a9eaf92
Kire1919 <131976754+Kire1919@users.noreply.github.com>
2023-05-01 22:48:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # Lista de nombres de árboles
 arboles = ["abedul", "abeto", "acacia", "acebo", "achicoria", "álamo", "árbol del pan", "arce", "avellano", "azufaifo", "baobab", "baño de oro", "bérbero", "brezo", "cactus", "caja", "calabaza", "camomila", "cedro", "cerezo", "cidro", "ciruela", "clavo", "coco", "corteza de roble", "corteza de sauce", "cresta de gallo", "crotón", "curuba", "dátil", "eucalipto", "falso pimentero", "fresno", "frutilla", "ginkgo", "girasol", "granado", "grosella espinosa", "guayaba", "henna", "hibisco", "higuera", "hinojo", "hisopo", "holly", "jacaranda", "jazmín", "jengibre", "jícara", "junco", "laurel", "lavanda", "lima", "lúpulo", "magnolia", "mandarina", "manzano", "maracuyá", "margarita", "menta", "mezquite", "mimosa", "mora", "moringa", "muérdago", "naranjo", "nogal", "olivo", "palma", "paraíso", "papaya", "peral", "perejil", "pino", "plátano", "plumeria", "pomelo", "poncirus", "porotillo", "quenepa", "quillay", "rábano", "rosa", "rosal silvestre", "sándalo", "sauce", "siempreviva", "spathodea", "tamarindo", "tangelo", "tejo", "temu", "tomate", "toronja", "uva", "vainilla", "vid", "yaca", "ylang ylang", "yuca"]
 
+
 def generar_nombre_arbol(nombre):
     suma_ascii = sum(ord(c) for c in nombre)
     indice_arbol = suma_ascii % len(arboles)
@@ -18,9 +19,14 @@ def obtener_nombre_arbol():
     nombre = request.args.get('nombre', '')
     nombre_arbol = generar_nombre_arbol(nombre)
     comando = ['python', 'arboles.py', nombre_arbol]
-    resultado = subprocess.check_output(comando, cwd='C:/miapp')
-    return jsonify({'nombre_arbol': nombre_arbol, 'resultado': resultado.decode()})
+    try:
+        resultado = subprocess.check_output(comando, cwd='C:/miapp')
+        return jsonify({'nombre_arbol': nombre_arbol, 'resultado': resultado.decode()})
+    except subprocess.CalledProcessError as error:
+        return jsonify({'error': str(error)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
