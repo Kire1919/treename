57755bb5bdfd6da54c99ad74742523eadfae5c5a
Kire1919 <131976754+Kire1919@users.noreply.github.com>
2023-05-02 14:45:45 +0200

    Update arbol.js

--- arbol.js
+++ arbol.js
@@ -1,6 +1,6 @@
 function obtenerArbol() {
     var nombre = document.getElementById("nombre").value;
-var url = "http://localhost:5000/arboles?nombre=" + nombre;
+var url = "http://127.0.0.1:5000/arboles?nombre=" + nombre;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
